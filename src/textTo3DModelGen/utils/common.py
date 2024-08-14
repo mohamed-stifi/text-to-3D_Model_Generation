1538aca9f57b0e6d5a3d6afab74fe17efccccd72
mohamed-stifi <mohamed.stifi.stifi@gmail.com>
2024-08-14 18:37:19 +0100

    utils added

---
+++ src/textTo3DModelGen/utils/common.py
@@ -0,0 +1,78 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from textTo3DModelGen import logger
+import json
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+import base64
+import torch.nn as nn
+import torch.optim as optim
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    try:
+        with open(path_to_yaml) as file:
+            content = yaml.safe_load(file)
+            logger.info(
+                f"yaml file: {path_to_yaml} loaded successfully"
+            )
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+    
+@ensure_annotations
+def create_directories(path_to_dirs: list, verbose=True):
+    for path in path_to_dirs:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+    logger.info(f"json file saved at: {path}")
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    with open(path) as f:
+        content = json.load(f)
+    
+    logger.info(f"json file loaded successfully from: {path}")
+    return ConfigBox(content)
+
+@ensure_annotations
+def save_csv(path: Path, data: dict):
+    pass
+
+@ensure_annotations
+def load_csv(path: Path) -> ConfigBox:
+    pass
+
+@ensure_annotations
+def save_model(path: Path, model: nn.Module, optimizer: optim.adam, info: dict):
+    pass
+
+@ensure_annotations
+def load_model(path: Path, model: nn.Module, optimizer: optim.adam) -> ConfigBox:
+    pass
+
+@ensure_annotations
+def save_mesh(path: Path, mesh):
+    pass
+
+@ensure_annotations
+def load_mesh(path: Path):
+    pass
+
+
+@ensure_annotations
+def get_size(path: Path):
+    size_in_mb = round(os.path.getsize(path)/1024**2)
+    return f"~ {size_in_mb} MB"
