54ba318a2f5b4f246817921c95feb55edc2d3a17
mohamed-stifi <mohamed.stifi.stifi@gmail.com>
2024-09-19 00:50:00 +0100

    training model stage added

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from textTo3DModelGen.pipeline.stage_01_data_ingestion import DataIngestionPipel
 from textTo3DModelGen.pipeline.stage_02_data_rendering import DataRenderingPipeline
 from textTo3DModelGen.pipeline.stage_03_text_embedding import TextEmbeddingPipeline
 from textTo3DModelGen.pipeline.stage_04_data_split import DataSplitPipeline
+from textTo3DModelGen.pipeline.stage_05_training_model import TrainingModelPipeline
 from textTo3DModelGen import logger
 import argparse
 
 
 parser = argparse.ArgumentParser(description="Run different stages of the process.")
 parser.add_argument("--stage", required=True, help="Specify the stage to run: \n1: run data ingestion pipeline.\n2: run data rendering pipeline.\n3: run text embedding pipeline.\
-                    \n3: run data split pipeline.\n5: run all pipeline.")
+                    \n3: run data split pipeline.\nn5: run training model pipeline.\n6: run all pipeline.")
 args = parser.parse_args()
 
 if __name__ == "__main__":
@@ -59,8 +60,20 @@ if __name__ == "__main__":
             logger.info(f">>>>>>>>> stage {STAGE_NAME} stoped -ERROR <<<<<<<<")
             logger.exception(e)
             raise e
+        
+    elif args.stage == "5":
+        STAGE_NAME = "Training Model stage"
+        try:
+            logger.info(f">>>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+            obj = TrainingModelPipeline()
+            obj.main()
+            logger.info(f">>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
+        except Exception as e:
+            logger.info(f">>>>>>>>> stage {STAGE_NAME} stoped -ERROR <<<<<<<<")
+            logger.exception(e)
+            raise e
 
     else:
-        print(f"{args.stage} is an invalid stage, should be in [1,2,3,4,5]")
+        print(f"{args.stage} is an invalid stage, should be in [1,2,3,4,5,6]")
 
 
