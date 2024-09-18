3b85ec5a2889d56b5e7e4bfe7a93d9e661a850b2
mohamed-stifi <mohamed.stifi.stifi@gmail.com>
2024-09-19 00:44:38 +0100

    training model config added

--- src/textTo3DModelGen/config/configuration.py
+++ src/textTo3DModelGen/config/configuration.py
@@ -5,6 +5,16 @@ from textTo3DModelGen.entity.config_entity import (DataIngestionConfig,
                                                    TextEmbeddingConfig,
                                                    DataSplitConfig)
 
+from textTo3DModelGen.entity.config_entity import (TrainingModelConfig,
+                                                   StyleGAN2Config,
+                                                   InferenceConfig,
+                                                   DatasetConfig,
+                                                   GeneratorConfig,
+                                                   LossAndDiscriminatorConfig,
+                                                   FeaturesConfig,
+                                                   HyperparametersConfig,
+                                                   SettingsConfig)
+
 
 class ConfigurationManager:
     def __init__(
@@ -14,6 +24,7 @@ class ConfigurationManager:
         
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
+        self.params = self.params.model_hyperprams
 
         create_directories([
             self.config.artifacts_root
@@ -86,4 +97,106 @@ class ConfigurationManager:
 
         return data_split_config
     
-    
+
+    def get_training_model_config(self) -> TrainingModelConfig:
+        config = self.config.training_prams
+        self.styleGAN2 = self.params.StyleGAN2
+        self.inference = self.params.inference
+        self.dataset = self.params.dataset
+        self.generator = self.params.generator
+        self.loss_and_discriminator = self.params.loss_and_discriminator
+        self.features = self.params.features
+        self.hyperparameters = self.params.hyperparameters
+        self.settings = self.params.settings
+
+        create_directories([
+            config.outdir  # , config.desc
+        ])
+
+        styleGAN2= StyleGAN2Config(
+            outdir = config.outdir,
+            cfg = self.styleGAN2.cfg,
+            gpus = config.gpus,
+            batch = config.batch,
+            gamma = config.gamma
+        )
+        inference= InferenceConfig(
+            resume_pretrain = self.inference.resume_pretrain,
+            inference_vis = self.inference.inference_vis,
+            inference_to_generate_textured_mesh = self.inference.inference_to_generate_textured_mesh,
+            inference_save_interpolation = self.inference.inference_save_interpolation,
+            inference_compute_fid = self.inference.inference_compute_fid,
+            inference_generate_geo = self.inference.inference_generate_geo
+        )
+        dataset= DatasetConfig(
+            data = config.data,
+            img_res = self.dataset.img_res,
+            data_split_file = config.data_split_file,
+            use_labels = self.dataset.use_labels
+        )
+        generator= GeneratorConfig(
+            iso_surface = self.generator.iso_surface,
+            use_style_mixing = self.generator.use_style_mixing,
+            one_3d_generator = config.one_3d_generator,
+            dmtet_scale = config.dmtet_scale,
+            n_implicit_layer = self.generator.n_implicit_layer,
+            feat_channel = self.generator.feat_channel,
+            mlp_latent_channel = self.generator.mlp_latent_channel,
+            deformation_multiplier = self.generator.deformation_multiplier,
+            tri_plane_resolution = self.generator.tri_plane_resolution,
+            n_views = self.generator.n_views,
+            use_tri_plane = self.generator.use_tri_plane,
+            tet_res = self.generator.tet_res,
+            latent_dim = self.generator.latent_dim,
+            geometry_type = self.generator.geometry_type,
+            render_type = self.generator.render_type
+        )
+        loss_and_discriminator= LossAndDiscriminatorConfig(
+            d_architecture = self.loss_and_discriminator.d_architecture,
+            use_pl_length = self.loss_and_discriminator.use_pl_length,
+            gamma_mask =self.loss_and_discriminator.gamma_mask,
+            d_reg_interval = self.loss_and_discriminator.d_reg_interval,
+            add_camera_cond = self.loss_and_discriminator.add_camera_cond,
+            lambda_flexicubes_surface_reg = self.loss_and_discriminator.lambda_flexicubes_surface_reg,
+            lambda_flexicubes_weights_reg =self.loss_and_discriminator.lambda_flexicubes_weights_reg
+        )
+        features= FeaturesConfig(
+            cond = self.features.cond,
+            freezed =self.features.freezed
+        )
+        hyperparameters= HyperparametersConfig(
+            batch_gpu = self.hyperparameters.batch_gpu,
+            cbase =self.hyperparameters.cbase,
+            cmax =self.hyperparameters.cmax,
+            glr = self.hyperparameters.glr,
+            dlr = self.hyperparameters.dlr,
+            map_depth = self.hyperparameters.map_depth,
+            mbstd_group =self.hyperparameters.mbstd_group
+        )
+        settings= SettingsConfig(
+            desc= config.desc ,
+            metrics=self.settings.metrics,
+            kimg= self.settings.kimg ,
+            tick=self.settings.tick ,
+            snap=self.settings.snap ,
+            seed=self.settings.seed,
+            fp32=self.settings.fp32 ,
+            nobench=self.settings.nobench,
+            workers=self.settings.workers ,
+            dry_run = self.settings.dry_run,
+
+        )
+
+        training_model_config = TrainingModelConfig(
+            styleGAN2 ,
+            inference,
+            dataset,
+            generator,
+            loss_and_discriminator,
+            features,
+            hyperparameters,
+            settings,
+        )
+
+        return training_model_config
+   
